f38cd36375112f3f767d14fe14789c93d22aff88
eric.w.nord <eric.w.nord@gmail.com>
2016-11-24 06:21:02 -0500

    enemy class working

--- spaceshooter/_js/enemy.js
+++ spaceshooter/_js/enemy.js
@@ -1,9 +1,11 @@
 class Enemy {
   constructor() {
     this.hitpoint = 1;
-    this.xPosition = 50;
-    this.yPosition = Math.random() * game.height - game.height / 2;
-    this.img = new Image('_img/enemy.png');
+    this.x = 50;
+    this.y = Math.random() * game.height - game.height / 2;
+    var enemyi = new Image();
+    enemyi.src = "_img/enemy.png";
+    this.image = enemyi;
     this.speed = -0.5;
   }
 }
