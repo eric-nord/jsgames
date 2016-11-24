adbc168d08f95216a839598a17ad858bf54c50d5
eric.w.nord <eric.w.nord@gmail.com>
2016-11-24 06:25:29 -0500

    enemy images cleaned up

--- spaceshooter/_js/enemy.js
+++ spaceshooter/_js/enemy.js
@@ -3,9 +3,8 @@ class Enemy {
     this.hitpoint = 1;
     this.x = 50;
     this.y = Math.random() * game.height - game.height / 2;
-    var enemyi = new Image();
-    enemyi.src = "_img/enemy.png";
-    this.image = enemyi;
+    this.image = new Image();
+    this.image.src = "_img/enemy.png";
     this.speed = -0.5;
   }
 }
